a0912b367f742d76200f26110ac653a39b486b18
sevvalkaradeniz <sevvalk@sabanciuniv.edu>
2022-11-01 12:37:10 +0300

    register page added

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,5 +2,52 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/signupmodels');
 
+router.post('/signup', (request, response) => {
+    const emaila = request.body.email;
+    const usernamea = request.body.username;
+    User.findOne({email: emaila}, function(err, docs){
+        if(err){
+            console.log(err);
+        }
+        else {
+            console.log("Result : ", docs);
+            if(docs == null)
+            {
+              User.findOne({username: usernamea}, function(err, docs){
+                if(err)
+                {
+                    console.log(err);
+                }
+                else
+                {
+                    if(docs == null)
+                    {
+                        const signedUpUser = new User({
+                            fullName: request.body.fullName,
+                            email: request.body.email,
+                            username: request.body.username,
+                            password: request.body.password
+                        })
+                        signedUpUser.save(function(err, user){
+                            if(err)
+                                response.json("Can't leave fields empty");
+                            else 
+                              response.json(signedUpUser);
+
+                        })
+                    }
+                    else
+                        response.send("Username taken");
+                }
+              })
+            }
+            else
+            {
+                response.send("Email already used.");
+            }
+        }
+    });
+})
+
 
 module.exports = router;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,56 @@
-export const Register = (params) => {
-    return <>Register</>
+import React, {useState} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from 'axios'
+import { useNavigate } from "react-router-dom"
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(pass);
+        console.log(name);
+        console.log(username);
+
+        const registered = {
+            fullName:name,
+            username: username,
+            email: email,
+            password: pass
+        }
+
+        axios.post('http://localhost:4000/app/signup', registered)
+        .then(response => {
+            if (response.data === "Username taken" || response.data === "Email already used." || response.data === "Can't leave fields empty")
+                alert(response.data);
+            else 
+                navigate("/settings/user",  {state: response.data});
+        }
+        )
+        
+        
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Register</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input value={name} onChange = {(e) => setName(e.target.value)} name="name" id="name" placeholder="full Name" />
+            <label htmlFor="name">Username</label>
+            <input value={username} onChange = {(e) => setUsername(e.target.value)} name="username" id="username" placeholder="username" />
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <button type="submit">Register</button>
+        </form>
+        <button className="link-btn" onClick={() => navigate('/login')}>Already have an account? Login here.</button>
+    </div>
+    )
 }

0191a48da80d16d3123f52919ce0b866d610b09d
Emirhan Ozdemir <emirhanozdemir2001@gmail.com>
2022-11-02 14:48:16 +0300

    bu ders katalog olmaz

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,4 @@ mongoose.connect("mongodb+srv://ghost:ghost@afra.qaclfcn.mongodb.net/myUsers",()
 app.use(express.json()); //-> body parser
 app.use(cors());
 app.use('/app', routeUrls); //-> /app is the base path and routeUrls will be appemded to it
-app.listen(process.env.PORT, () => console.log("Server is running"));
+app.listen(process.env.PORT || 4000, () => console.log("Server is running"));

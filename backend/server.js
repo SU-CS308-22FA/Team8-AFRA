842a8bcfa67050b81e1868ae23b6efa1692762ee
Emirhan Ozdemir <emirhanozdemir2001@gmail.com>
2022-11-02 15:43:34 +0300

    b

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,4 @@ if(process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(PORT, () => console.log(`Server is running ${port}`));
+app.listen(PORT, () => console.log(`Server is running ${PORT}`));

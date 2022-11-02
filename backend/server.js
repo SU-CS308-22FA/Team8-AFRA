e0b2472e2489e957715816fe8b56bca003ad4f7a
Emirhan Ozdemir <emirhanozdemir2001@gmail.com>
2022-11-02 14:59:46 +0300

    olmuyordu

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const router = require("./routes/routes");
 const routeUrls = require('./routes/routes');
+const PORT = process.env.PORT || 4000;
 const dotenv = require('dotenv');
 const cors = require('cors');
 dotenv.config({path: "./config.env"});
@@ -12,4 +13,4 @@ mongoose.connect("mongodb+srv://ghost:ghost@afra.qaclfcn.mongodb.net/myUsers",()
 app.use(express.json()); //-> body parser
 app.use(cors());
 app.use('/app', routeUrls); //-> /app is the base path and routeUrls will be appemded to it
-app.listen(process.env.PORT || 4000, () => console.log("Server is running"));
+app.listen(PORT, () => console.log(`Server is running ${port}`));

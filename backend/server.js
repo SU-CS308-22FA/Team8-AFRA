c30f61876a55400c2fdd64a3c4d32d91c0cef18d
Emirhan Ozdemir <emirhanozdemir2001@gmail.com>
2022-11-03 13:41:17 +0300

    start scripts are changing
    node_env expression changed
    deployment will be tried

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,16 @@ mongoose.connect("mongodb+srv://ghost:ghost@afra.qaclfcn.mongodb.net/myUsers",()
 app.use(express.json()); //-> body parser
 app.use(cors());
 app.use('/app', routeUrls); //-> /app is the base path and routeUrls will be appemded to it
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    console.log("Its production mode");
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+   // app.use('public', express.static('../public'))
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve('../public/index.html'));
+    });
+}
+
 app.listen(4000, () => console.log("Server is running"));

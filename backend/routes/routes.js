9cae8f6da70986dc8402767dd5a0a2e99e7bfcb7
zeynepturkmen <zeynepturkmen@sabanciuniv.edu>
2022-11-11 21:05:42 +0300

    adds the requests to the request collection and the users to myUsers along with the google drive link

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../models/signupmodels');
-const Prof = ('../models/profmodels');
+const Request =  require('../models/requestmodel');
+//google drive stuff
 const multer = require("multer");
 const upload = multer({ dest: "public/files" });
 const uploadSingleFile = require('../upload');
+const deleteDriveFile = require('../delete');
 const fs = require("fs");
 
-const deleteFile = (filePath) => {
+const deleteFile = (filePath) => { //delete file from local directory
     fs.unlink(filePath, () => {
       console.log("file deleted");
     });
   };
 
-
-router.post('/signup', (request, response) => {
+  router.post('/signup', (request, response) => {
     const emaila = request.body.email;
     const usernamea = request.body.username;
     User.findOne({email: emaila}, function(err, docs){
@@ -61,6 +62,64 @@ router.post('/signup', (request, response) => {
     });
 })
 
+
+router.post('/prosignup', (request, response) => {
+    const emaila = request.body.email;
+    const usernamea = request.body.username;
+    User.findOne({email: emaila}, function(err, docs){
+        if(err){
+            console.log(err);
+        }
+        else {
+            console.log("Result : ", docs);
+            if(docs == null)
+            {
+              User.findOne({username: usernamea}, function(err, docs){
+                if(err)
+                {
+                    console.log(err);
+                }
+                else
+                {
+                    if(docs == null)
+                    {
+                        let l = "https://drive.google.com/file/d/" + request.body.licence;
+                        const signedUpUser = new User({
+                            fullName: request.body.fullName,
+                            email: request.body.email,
+                            username: request.body.username,
+                            password: request.body.password,
+                            licence: l,
+                            accepted: false
+                        })
+                        signedUpUser.save(function(err, user){
+                            if(err)
+                                response.json("Can't leave fields empty");
+                            else 
+                            {
+                                const proReq = new Request({
+                                    email: request.body.email,
+                                    licence: l,
+                                    accepted: false
+                                })
+                                proReq.save();
+                                response.json(signedUpUser);  
+                            } 
+                        })
+                    }
+                    else
+                        response.send("Username taken");
+                }
+              })
+            }
+            else
+            {
+                response.send("Email already used.");
+            }
+        }
+    });
+})
+
 router.post('/signin', (request, response) => {
     const emaila = request.body.email;
     User.findOne({email: emaila}, function(err, docs){
@@ -75,8 +134,15 @@ router.post('/signin', (request, response) => {
             {
                 const pass = request.body.password;
                 if(docs.password === pass) {
-                    console.log("Im sending the docs");
-                    response.json(docs); 
+
+                    //check if the user is accepted pro
+                    if (docs.accepted !== false)
+                    {
+                        console.log("Im sending the docs");
+                        response.json(docs); 
+                    }
+                    else
+                        response.send("Your licence is not verified yet!");
                 }   
                 else
                     response.send("wrong password");
@@ -88,15 +154,25 @@ router.post('/signin', (request, response) => {
 router.post("/google-drive", upload.single("file"), async (req, res, next) => {
     try {
         if (!req.file) {
-          res.status(400).send("No file uploaded.");
+          res.send("No file uploaded.");
           return;
         }
         const response = await uploadSingleFile(req.file);
         deleteFile(req.file.path);
-        res.status(200).json({ response });
+        console.log(response.data.id);
+        res.json({ id: response.data.id });
       } catch (err) {
         console.log(err);
-  }});
+  }
+});
+
+router.post('/drivedelete', (req, res) => {
+    var fileId = req.body.licence; // Desired file id to download from  google drive
+    console.log("This is drive delete");
+    const response = deleteDriveFile(fileId);
+    res.send(response.data);
+  });
+
 
   router.post('/userSettings', (request, response) => {
     const emaila = request.body.email;
@@ -123,8 +199,7 @@ router.post("/google-drive", upload.single("file"), async (req, res, next) => {
                     username: request.body.username}, function(err, docs){
                     console.log(docs);
                 });
-               
-                response.send("done");
+                response.send("Done");
             }
         }
     });

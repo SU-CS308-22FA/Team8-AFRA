546470a373877d099a32de18fbc871751d278113
Kaan Adalılar <kaanadalilar@sabanciuniv.edu>
2022-11-21 20:51:33 +0300

    routes_contr

--- backend/controllers/matchControllers.js
+++ backend/controllers/matchControllers.js
@@ -69,4 +69,26 @@ const getMatchesBySeasonAndWeek = asyncHandler(async (req, res) => {
   res.json(r);
 });
 
-export { uploadDatabase, getMatchesBySeasonAndWeek };
+const getStandingsBySeason = asyncHandler(async (req, res) => {
+  const { season } = req.query;
+  const options = {
+    method: "GET",
+    url: "https://api-football-v1.p.rapidapi.com/v3/standings",
+    params: { season: season, league: "203" },
+    headers: {
+      "X-RapidAPI-Key": process.env.X_RAPIDAPI_KEY,
+      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (r) {
+      res.json(r.data.response[0].league.standings[0]);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+export { uploadDatabase, getMatchesBySeasonAndWeek, getStandingsBySeason };
--- backend/routes/matchRoutes.js
+++ backend/routes/matchRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   uploadDatabase,
   getMatchesBySeasonAndWeek,
+  getStandingsBySeason
 } from "../controllers/matchControllers.js";
 
 const router = express.Router();
 
 router.route("/").post(uploadDatabase);
 router.route("/fixture").get(getMatchesBySeasonAndWeek);
+router.route("/standings").get(getStandingsBySeason);
 
 export default router;

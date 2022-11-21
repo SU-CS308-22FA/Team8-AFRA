5ee4aa91697f818176ce1ca3c12e1f5f835fe29f
Kaan Adalılar <kaanadalilar@sabanciuniv.edu>
2022-11-21 23:10:59 +0300

    role working

--- backend/controllers/requestController.js
+++ backend/controllers/requestController.js
@@ -9,4 +9,27 @@ const getAllRequests = asyncHandler(async (req, res) => {
     console.log(data)
     res.json(data);
 })
-export { getAllRequests};
+
+const verifyUser = asyncHandler(async (req, res) => {
+    //console.log(req.body);
+    const proreq = await proRequest.findById(req.body.id);
+    //console.log(proreq);
+    const user = await User.findById(proreq.user)
+    //console.log(user);
+    
+    //console.log(request);
+    if(req.body.role !== "deny")
+   {
+      user.role = req.body.role;
+      user.accepted = true;
+      User.updateOne({_id:proreq.user},{role:user.role, accepted:user.accepted}, function(err, docs){
+        if(err)
+        console.log(err)
+        else
+            console.log(docs)
+      });
+   } 
+   const request = await proRequest.deleteOne({_id: req.body.id})
+   res.status(200);
+})
+export { getAllRequests, verifyUser};
--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {
- getAllRequests,
+ getAllRequests, verifyUser
 } from "../controllers/requestController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllRequests);
+router.route("/verify").post(verifyUser);
 
 export default router;

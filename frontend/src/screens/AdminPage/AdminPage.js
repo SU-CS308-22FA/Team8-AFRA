77e8d1611806b44dcba1abacea67e321e0363566
Kaan Adalılar <kaanadalilar@sabanciuniv.edu>
2022-11-21 20:44:19 +0300

    form added

--- frontend/src/screens/AdminPage/AdminPage.js
+++ frontend/src/screens/AdminPage/AdminPage.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Card, Dropdown, DropdownButton, Row, Col, Table, Form, Button } from "react-bootstrap";
 import axios from "axios";
 function AdminPage() {
-  const [seasonVar, setSeasonVar] = useState("2021");
-  const [message, setMessage] = useState("");
+  const [seasondata, setSeasonData] = useState([]);
+  const [requestdata, setRequestData] = useState([]);
+  const [seasonVar, setSeasonVar] = useState();
+  const [message, setMessage] = useState();
 
-  const submitHandler = async (e) => {
+  const handleSelectSeason=(e)=>{
+    setSeasonVar(e);
+  }
+
+  const submitSeasonHandler = async (e) => {
     e.preventDefault();
     const config = {
       headers: {
         "Content-type": "application/json",
       },
     };
-
+    console.log(process.env.REACT_APP_URL);
     try {
-      const { data } = await axios.post(
+      const { seasondata } = await axios.post(
         `${process.env.REACT_APP_URL}/api/matches`,
         {
           seasonVar,
@@ -26,26 +32,89 @@ function AdminPage() {
     }
 
     console.log(message);
+  };
+
+  const submitRequestHandler = async (e) => {
+    console.log("submitted");
+    
+    e.preventDefault();
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+
+    try {
+      const { requestdata } = await axios.get(`${process.env.REACT_APP_URL}/api/requests`);
+      console.log(requestdata);
+    } catch (error) {}
+
   };
 
   return (
-    <div>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Season</Form.Label>
-          <Form.Control
-            value={seasonVar}
-            onChange={(e) => setSeasonVar(e.target.value)}
-          />
-        </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-      <div>{message}</div>
+    <div>
+      <Row>
+        <Col>
+        <Card.Body>
+          <Card.Title>GET REQUESTS</Card.Title>
+          <Card.Text> To see the current requests press Get Requests </Card.Text>
+          <Form onSubmit={submitRequestHandler} className="getSubmit">
+            <Button variant="primary" type="submit"> Get Requests </Button>
+          </Form>
+        </Card.Body>
+        <Table responsive>
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>License</th>
+            <th>Accept/Reject</th>
+        </tr>
+      </thead>
+      <tbody>
+        {requestdata.map(requestdata=>{
+          return(
+          <tr>
+            <td>{requestdata.name}</td>
+            <td>{requestdata.username}</td>
+            <td>{requestdata.email}</td>
+            <td>{requestdata.license}</td>
+          </tr>
+          )
+        }
+        )}
+      </tbody>
+      </Table>
+      </Col>
+      
+      <Col>
+      <Card.Body>
+        <Card.Title>GET SEASON TO UPLOAD IT TO DATABASE</Card.Title>
+        <Card.Text> To get the season enter a year</Card.Text>
+        <div className="dropdown">
+          <DropdownButton id="dropdown-basic-button" title="Choose a season" onSelect={handleSelectSeason}>
+          <Dropdown.Item eventKey="2022">2022</Dropdown.Item>
+          <Dropdown.Item eventKey="2021">2021</Dropdown.Item>
+          <Dropdown.Item eventKey="2020">2020</Dropdown.Item>
+          <Dropdown.Item eventKey="2019">2019</Dropdown.Item>
+          <Dropdown.Item eventKey="2018">2018</Dropdown.Item>
+          <Dropdown.Item eventKey="2017">2017</Dropdown.Item>
+          <Dropdown.Item eventKey="2016">2016</Dropdown.Item>
+          <Dropdown.Item eventKey="2015">2015</Dropdown.Item>
+          <Dropdown.Item eventKey="2014">2014</Dropdown.Item>
+          <Dropdown.Item eventKey="2013">2013</Dropdown.Item>
+          <Dropdown.Item eventKey="2012">2012</Dropdown.Item>
+        </DropdownButton>
+      </div>
+          <Form onSubmit={submitSeasonHandler} className="getSubmit">
+            <Button variant="primary" type="submit"> Get Season </Button>
+          </Form>
+          </Card.Body></Col>
+      </Row>
+      
     </div>
   );
 }
-
 export default AdminPage;

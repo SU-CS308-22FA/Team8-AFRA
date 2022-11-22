2b8712aaf20f780ab016d7a98890b59218105b19
Kaan Adalılar <kaanadalilar@sabanciuniv.edu>
2022-11-22 11:14:51 +0300

    env  variable fix

--- frontend/src/screens/StandingPage/StandingPage.js
+++ frontend/src/screens/StandingPage/StandingPage.js
@@ -23,7 +23,7 @@ function StandingPage() {
       };
   
       const { data } = await axios.get(
-        "http://localhost:4000/api/matches/standings",
+        `${process.env.REACT_APP_URL}/api/matches/standings`,
         {
           params: {
             season: seasonVar,

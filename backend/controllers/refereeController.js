e8debcec85bfe5b7e0cb5144f3817e5a46288004
Kaan Adalılar <kaanadalilar@sabanciuniv.edu>
2022-12-09 23:02:11 +0300

    referee details redirected
    one can go to a detail about single referee by clicking the referee name

--- backend/controllers/refereeController.js
+++ backend/controllers/refereeController.js
@@ -2,8 +2,24 @@ import Referee from "../models/refereeModel.js";
 import asyncHandler from "express-async-handler";
 
 const getAllReferees = asyncHandler(async (req, res) => {
-  const referees = await Referee.find(); //{ user: req.user._id }
+  const referees = await Referee.find().sort({ matchCount: -1 }); //{ user: req.user._id }
   res.json(referees);
 });
+
+/*
+const getRefereeById = asyncHandler(async (req, res) => {
+  const referee = await Referee.findById(req.params.id);
+
+  if (referee) {
+    res.json(referee);
+  } else {
+    res.status(404).json({ message: "Referee not found" });
+  }
+
+  res.json(referee);
+});
+
+export { getAllReferees, getRefereeById };
+*/
 
 export { getAllReferees };
--- backend/routes/refereeRoutes.js
+++ backend/routes/refereeRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 
-import { getAllReferees } from "../controllers/refereeController.js";
+import {
+  getAllReferees,
+  //getRefereeById,
+} from "../controllers/refereeController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllReferees);
+//router.route("/:id").get(getRefereeById);
 
 export default router;

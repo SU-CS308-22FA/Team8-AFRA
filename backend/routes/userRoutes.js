583c91f315fc426e5540b18c0fad890f0b09f839
zeynepturkmen <zeynepturkmen@sabanciuniv.edu>
2022-12-10 20:49:48 +0300

    Updates: Updated react version and fixed the deprecated components, switched history for navigate
    Google Calendar Feature: added an authentication path so that only the logged in users can access
    
    Added Admin Ban Page: lists all the reports done by the users, admins can choose to ban or not.
    
    Added Authentication path: All dedicated user routes are wrapped by authentication that checks the ban status with each request
    
    Authentication Fail pages: For trying to access the admin page, or the user pages without tokens

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,15 @@ import {
   authUser,
   deleteUserProfile,
   registerUser,
-  updateUserProfile
+  updateUserProfile,
+  checkBanned
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
+router.get("/check-banned", checkBanned);
 router.route("/profile").post(protect, updateUserProfile);
 router.route("/deleteaccount").post(protect, deleteUserProfile);
 

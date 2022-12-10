2bac87d620546ea221e0ba6154580dd4623f3ae6
Kaan Adalılar <kaanadalilar@sabanciuniv.edu>
2022-12-10 18:51:25 +0300

    referee sort feature addes
    referees can be sorted according to field

--- backend/routes/refereeRoutes.js
+++ backend/routes/refereeRoutes.js
@@ -2,12 +2,15 @@ import express from "express";
 
 import {
   getAllReferees,
-  //getRefereeById,
+  getRefereeNameSorted,
+  getRefereeRankSorted,
+  getRefereeMatchCountSorted,
 } from "../controllers/refereeController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllReferees);
-//router.route("/:id").get(getRefereeById);
-
+router.route("/sortbyname").get(getRefereeNameSorted);
+router.route("/sortbyrank").get(getRefereeRankSorted);
+router.route("/sortbymatchcount").get(getRefereeMatchCountSorted);
 export default router;

0391e6dcd285c6e3ab083b90143a7d2fc007a5e0
Kaan Adalılar <kaanadalilar@sabanciuniv.edu>
2022-12-10 18:51:25 +0300

    referee sort feature addes
    referees can be sorted according to field

--- frontend/src/screens/RefereesScreen/RefereesScreen.js
+++ frontend/src/screens/RefereesScreen/RefereesScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Form, Button, Table } from "react-bootstrap";
+import { Form, Button, Table, Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "./RefereesScreen.css";
@@ -7,6 +7,51 @@ import "./RefereesScreen.css";
 function RefereesScreen() {
   const [data, setData] = useState([]);
   const [tableHead, setTableHead] = useState([]);
+
+  const sortByRank = async () => {
+    axios
+      .get(`${process.env.REACT_APP_URL}/api/referees/sortbyrank`)
+      .then((res) => {
+        const referees = res.data;
+        setData(referees);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const sortByName = async () => {
+    axios
+      .get(`${process.env.REACT_APP_URL}/api/referees/sortbyname`)
+      .then((res) => {
+        const referees = res.data;
+        setData(referees);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const sortByMatchCount = async () => {
+    axios
+      .get(`${process.env.REACT_APP_URL}/api/referees/sortbymatchcount`)
+      .then((res) => {
+        const referees = res.data;
+        setData(referees);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const sortByDefault = async () => {
+    axios
+      .get(`${process.env.REACT_APP_URL}/api/referees/sortbymatchcount`)
+      .then((res) => {
+        const referees = res.data;
+        setData(referees);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
     const newHead = [
@@ -35,6 +80,35 @@ function RefereesScreen() {
       <h1 className="mainTitle">Referees in Super League</h1>
       <p> </p>
 
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <Dropdown>
+                <Dropdown.Toggle variant="light" id="dropdown-basic">
+                  Sort Referees
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => sortByRank()}>
+                    Sort by Rank (Most to Least)
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={() => sortByName()}>
+                    Sort by Name(Alphabetically A to Z)
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={() => sortByMatchCount()}>
+                    Sorted by Match Count(Most to Least)
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={() => sortByDefault()}>
+                    Default (Sorted by Match Count)
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </th>
+          </tr>
+        </thead>
+      </table>
+
       <Table responsive>
         <thead>
           <tr>
@@ -58,7 +132,7 @@ function RefereesScreen() {
           {data.map((data) => {
             return (
               <tr>
-                <td>{10}</td>
+                <td>{data.rank}</td>
                 <td>
                   <a href={`/referee/${data.name}`}>{data.name}</a>
                 </td>

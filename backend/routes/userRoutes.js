39bf9124f0ad4661d170dd330aa39876603905fa
sevvalkaradeniz <sevvalk@sabanciuniv.edu>
2022-12-11 20:37:43 +0300

    verificationadded

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,8 @@ import {
   deleteUserProfile,
   registerUser,
   updateUserProfile,
+  sendOTPVerificationEmail,
+  VerifyOTP,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
@@ -12,5 +14,8 @@ router.route("/").post(registerUser);
 router.post("/login", authUser);
 router.route("/profile").post(protect, updateUserProfile);
 router.route("/deleteaccount").post(protect, deleteUserProfile);
+router.route("/sendotpmessage").post(sendOTPVerificationEmail);
+router.route("/verifyotp").post(VerifyOTP);
+
 
 export default router;
--- frontend/src/screens/VerificationPage/VerificationPage.js
+++ frontend/src/screens/VerificationPage/VerificationPage.js
@@ -4,18 +4,20 @@ import { useDispatch, useSelector } from "react-redux";
 import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 import MainScreen from "../../components/MainScreen";
-import {updateProfile} from "../../actions/userActions";
+import {updateProfile,sendOTPmail,verifyOTPmail} from "../../actions/userActions";
 import axios from 'axios'
 
 const VerificationPage = ({ location, history }) => {
+   const pass="";
     const [selectedFile, setSelectedFile] = useState();
+    const [passOfOTP, SetPassOfOTP] = useState(pass);
     const dispatch = useDispatch();
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
   
     const userUpdate = useSelector((state) => state.userUpdate);
     const { loading, error, success } = userUpdate;
-    let verified = "";
+    let verified="";
 
     if(userInfo.verified)
         verified = "Verified"
@@ -71,8 +73,40 @@ const VerificationPage = ({ location, history }) => {
       
     };
   
+    const sendOTPmessage = async ()=>{
+      dispatch(sendOTPmail(userInfo));
+    }
 
+    const verifyOTPmessage =  (otp)=>{
+      
+      //await dispatch(verifyOTPmail(otp));
   
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+      const userId=userInfo._id;
+      axios.post(`${process.env.REACT_APP_URL}/api/users/verifyotp`, {userId: userId,otp :otp}).then(response=>{
+        console.log(response.data);
+        if(response.data==="VERIFIED"){
+          console.log("verification done")
+          let verified=true;
+          let _id= userInfo._id;
+          dispatch(updateProfile({_id,verified}))
+        }
+        
+      }).catch((err)=>{
+        console.log(err);
+      })
+    
+      //window.location.reload();
+    }
+
+
+
+    
     return (
       <MainScreen title="VERIFY YOUR ACCOUNT">
         <div>
@@ -93,10 +127,29 @@ const VerificationPage = ({ location, history }) => {
                 <br></br>
                 <h5> Status:   <b>{userInfo.role}</b> </h5>
                 <br></br>
-                <h5> Your account is:   <b>{verified}</b> </h5>
-                <Button type="submit" varient="primary">
+            
+                <h5> Your account is:   <b>{userInfo.verified===true? "verified": "Not verified yet!"}</b> </h5>
+                {verified ? (
+                  <Form onSubmit={emailHandler}>
+                  <Button type="submit" varient="primary" onClick={()=> sendOTPmessage()}>
                   Send a verification email
                 </Button>
+                <Form.Group controlId="passOfUser">
+                <Form.Label>Enter One Time Password </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter One Time Password"
+                  value={passOfOTP}
+                  onChange={(e) => SetPassOfOTP(e.target.value)}
+                ></Form.Control>
+                </Form.Group>
+                
+                <Button type="submit" varient="primary" onClick={()=>{verifyOTPmessage(passOfOTP)}}>
+                Check OTP
+              </Button>
+              </Form>
+                ) : <></>}
+                
               </Form>
             </Col>
 

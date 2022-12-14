38e98444cd54495d92907dee02b554de0bded6e9
Zeynep Türkmen <75041108+zeynepturkmen@users.noreply.github.com>
2022-12-14 20:16:31 +0300

    bug fix for calendar api, added try catch for errors

--- backend/routes/calenderRoutes.js
+++ backend/routes/calenderRoutes.js
@@ -18,17 +18,20 @@ function addHoursToDate(date, hours) {
 }
 
 router.post("/", async(req, res, next) => {
-    try{
-      const {code, user} = req.body
-      const {tokens} = await oauth2Client.getToken(code);
-      const theuser = await User.findById(user);
-      theuser.refresh_token = tokens.refresh_token || theuser.refresh_token;
+  try{
+    const {code, user} = req.body
+    const {tokens} = await oauth2Client.getToken(code);
+    const theuser = await User.findById(user);
+    if(!theuser.refresh_token)
+    {
+      theuser.refresh_token = tokens.refresh_token;
       const yes = await theuser.save();
-      res.send("SUCCESS")
     }
-    catch(err){
-      console.log(err)
-    }
+    res.send("SUCCESS")
+  }
+  catch(err){
+    console.log(err)
+  }
 })
 
 router.post("/create-event", async(req, res, next) => {

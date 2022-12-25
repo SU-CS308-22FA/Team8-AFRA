b90c74e129f6ad61d9e4c753a46a399653ac5194
sevvalkaradeniz <sevvalk@sabanciuniv.edu>
2022-12-25 23:02:08 +0300

    UpdatedEdixFixture
    Small bug solved

--- frontend/src/screens/FixturePage/FixturePage.js
+++ frontend/src/screens/FixturePage/FixturePage.js
@@ -243,6 +243,7 @@ let weeksOfTheMatches=["1","2","3","4","5","6","7","8","9","10","11","12",
                                      <Form.Group controlId= "getSelectionForUpdateWeek">
                                         <Form.Check 
                                           type='radio'
+                                          name= "belongSameRadio"
                                           id={`default-radio`}
                                           label={labelName}
                                           value= {labelName}

b34c5d5dee8b7ea5933833289416ca3451fbe161
Kaan Adalılar <kaanadalilar@sabanciuniv.edu>
2023-01-02 21:15:07 +0300

    Kaan test files added

---
+++ zey_selen/Kaan_Tests/Test1.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager    
+
+class TestRecommendation(unittest.TestCase):
+    
+    def setUp(self):
+       self.driver = webdriver.Chrome(ChromeDriverManager().install())
+       self.driver.maximize_window()
+       self.driver.get("https://coolafra.herokuapp.com/")
+       time.sleep(1)
+
+    def test_recommendation(self):
+        #- - - - - - - - - -ALL FIELDS ARE FILLED CASE- - - - - - - - - -
+        self.driver.find_element(By.XPATH,"//*[@id=\"root\"]/div/div/div/div/div[2]/a[1]/button").click()
+        time.sleep(1)
+        mail = self.driver.find_element(By.XPATH, '//*[@id="formBasicEmail"]')
+        mail.send_keys("kaancadalilar@gmail.com")
+        time.sleep(1)
+        password = self.driver.find_element(By.XPATH, '//*[@id="formBasicPassword"]')
+        password.send_keys("kaann")
+        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/div/form/button').click()
+        time.sleep(1)
+        print("Login achieved.✅")
+        self.driver.find_element(By.XPATH,"//*[@id=\"responsive-navbar-nav\"]/div[2]/a").click()
+        self.driver.find_element(By.XPATH,"//*[@id=\"dropdown-button\"]").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH,"//*[@id=\"root\"]/div/div[2]/div/div[1]/div[2]/form/div[1]/div/div/div/a[7]").click()
+        detail = self.driver.find_element(By.XPATH, '//*[@id="formBasic"]')
+        detail.send_keys("Referees page is being loaded slowly.")
+        time.sleep(1)
+        self.driver.find_element(By.XPATH,"//*[@id=\"root\"]/div/div[2]/div/div[1]/div[2]/form/button").click()
+        time.sleep(1)
+        isSuccess = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[1]/div[2]/div/strong')
+        print(isSuccess)
+        self.assertTrue(isSuccess, "Bug Report is added!")
+        print("1/2 User can send the report by clicking the submit button, first test has passed. ✅")
+        time.sleep(2)
+        #- - - - - - - - - -EMPTY FIELD CASE- - - - - - - - - -
+        self.driver.get("https://coolafra.herokuapp.com/")
+        time.sleep(1)
+        self.driver.find_element(By.XPATH,"//*[@id=\"responsive-navbar-nav\"]/div[2]/a").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH,"//*[@id=\"dropdown-button\"]").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH,"//*[@id=\"root\"]/div/div[2]/div/div[1]/div[2]/form/div[1]/div/div/div/a[7]").click()
+        self.driver.find_element(By.XPATH,"//*[@id=\"root\"]/div/div[2]/div/div[1]/div[2]/form/button").click()
+        time.sleep(1)
+        isSuccess = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[1]/div[2]/div/strong')
+        self.assertTrue(isSuccess, "Fields cannot be empty.")
+        print("2/2 User cannot send the report when does not enter a bug detail, second test has passed. ✅")
+        time.sleep(2)
+        
+if __name__ == "__main__":
+    unittest.main()
---
+++ zey_selen/Kaan_Tests/Test2.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager    
+
+class TestRecommendation(unittest.TestCase):
+    
+    def setUp(self):
+       self.driver = webdriver.Chrome(ChromeDriverManager().install())
+       self.driver.maximize_window()
+       self.driver.get("https://coolafra.herokuapp.com/")
+       time.sleep(1)
+
+    def test_recommendation(self):
+        #- - - - - - - - - -SELECT A TEAM FROM 2022- - - - - - - - - -
+        self.driver.find_element(By.XPATH,"//*[@id=\"responsive-navbar-nav\"]/div[5]/a[1]").click()
+        time.sleep(2)
+        selectedTeam = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[4]/div/div[2]/div")
+        self.driver.find_element(By.XPATH,"//*[@id=\"root\"]/div/div[2]/div[4]/div").click()
+        time.sleep(2)
+        currentTeam = self.driver.find_element(By.XPATH, '//*[@id="c-info-head"]')
+        self.assertTrue(selectedTeam, currentTeam)
+        print("1/2 User can go to a team detail page of a team of current season, first test has passed. ✅")
+        time.sleep(2)
+        #- - - - - - - - - -SELECT ANOTHER SEASON- - - - - - - - - -
+        self.driver.get("https://coolafra.herokuapp.com/")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH,"//*[@id=\"responsive-navbar-nav\"]/div[5]/a[1]").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH,"//*[@id=\"dropdown-basic-button\"]").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH,"//*[@id=\"root\"]/div/div[1]/div/div/div/div/a[5]").click()
+        time.sleep(2)
+        selectedTeam = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[8]/div/div[2]/div")
+        self.driver.find_element(By.XPATH,"//*[@id=\"root\"]/div/div[2]/div[8]/div").click()
+        time.sleep(2)
+        currentTeam = self.driver.find_element(By.XPATH, '//*[@id="c-info-head"]')
+        self.assertTrue(selectedTeam, currentTeam)
+        print("2/2 User can go to a team detail page of a team of selected season (2018 in this case), first test has passed. ✅")
+        time.sleep(2)
+        
+if __name__ == "__main__":
+    unittest.main()

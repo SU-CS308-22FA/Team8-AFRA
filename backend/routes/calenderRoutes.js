5564806be3baefbb2b8615a6d8f46ed2c2e858bc
Zeynep Türkmen <75041108+zeynepturkmen@users.noreply.github.com>
2023-01-04 13:35:01 +0300

    typo

--- backend/routes/calenderRoutes.js
+++ backend/routes/calenderRoutes.js
@@ -22,7 +22,7 @@ router.post("/", async(req, res, next) => {
     const {code, user} = req.body
     const {tokens} = await oauth2Client.getToken(code);
     const theuser = await User.findById(user);
-    if(tokens.refresh_token && user.refresh_token !== tokens.refresh_token)
+    if(tokens.refresh_token && theuser.refresh_token !== tokens.refresh_token)
     {
       theuser.refresh_token = tokens.refresh_token;
       const yes = await theuser.save();
